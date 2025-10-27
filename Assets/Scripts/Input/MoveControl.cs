da28cb43cbd47b02d3190b21ee49218eca50afe1
yuji2005yy <yuji2005yy@gmail.com>
2025-10-27 19:07:53 +0900

    [Add] New Input Action Mapを使って試験的に移動入力を実装

---
+++ Assets/Scripts/Input/MoveControl.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class MoveControl : MonoBehaviour
+{
+    [SerializeField] private float _speed = 5;
+    [SerializeField] private float _jumpForce = 5;
+
+    private InputSystem_Actions _InputSystem;
+    private Rigidbody _rigidbody;
+    private Vector2 _moveInputValue;
+
+    private void Awake()
+    {
+        //インスタンス化
+        _rigidbody = GetComponent<Rigidbody>();
+        _InputSystem = new InputSystem_Actions();
+
+        // Actionイベント登録
+        _InputSystem.Player.Move.started += OnMove;
+        _InputSystem.Player.Move.performed += OnMove;
+        _InputSystem.Player.Move.canceled += OnMove;
+        _InputSystem.Player.Jump.performed += OnJump;
+
+        // Input Actionを機能させるためには有効化する必要がある
+        _InputSystem.Enable();
+    }
+
+    private void OnDestroy()
+    {   //イベント情報を廃棄する
+        _InputSystem.Dispose(); 
+    }
+
+    private void OnMove(InputAction.CallbackContext context)
+    {
+        _moveInputValue = context.ReadValue<Vector2>();
+    }
+
+    private void OnJump(InputAction.CallbackContext context)
+    {
+        _rigidbody.AddForce(Vector3.up * _speed, ForceMode.Impulse);
+    }
+
+    private void FixedUpdate()
+    {
+        _rigidbody.AddForce(new Vector3(_moveInputValue.x, 0, _moveInputValue.y));
+    }
+}
