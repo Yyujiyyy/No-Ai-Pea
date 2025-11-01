dd2663f5f9dbff1325654e40a939221be0cd990e
yuji2005yy <yuji2005yy@gmail.com>
2025-11-01 19:22:14 +0900

    [Add] 視点操作スクリプトを作成

---
+++ Assets/Scripts/Input/AngleControler.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class AngleControler : MonoBehaviour
+{
+    private InputSystem_Actions _inputSystem;
+
+    private void Awake()
+    {
+        _inputSystem = new InputSystem_Actions();
+        _inputSystem.Player.Look.started += OnLook;
+        _inputSystem.Player.Look.performed += OnLook;
+        _inputSystem.Player.Look.canceled += OnLook;
+    }
+
+    private void OnLook(InputAction.CallbackContext context)
+    {
+        
+    }
+}
