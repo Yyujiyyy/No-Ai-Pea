ce1bf2d5f95c3329d116e8ce4e06a8c4e365e812
yuji2005yy <yuji2005yy@gmail.com>
2025-11-11 21:43:43 +0900

    [Add] 視点操作を試行錯誤中

--- Assets/Scripts/Input/AngleControler.cs
+++ Assets/Scripts/Input/AngleControler.cs
@@ -14,6 +14,8 @@ public class AngleControler : MonoBehaviour
     public float premouseX; 
     public float premouseY;
 
+    public float Qx, Qy, Qz;
+
     private void Awake()
     {
         _tr = transform;
@@ -34,14 +36,27 @@ public class AngleControler : MonoBehaviour
 
     private void OnLook(InputAction.CallbackContext context)
     {
-        _mouseDifference = context.ReadValue<Vector2>();
+        _mouseDifference += context.ReadValue<Vector2>();
     }
     
     private void Update()
     {
+        Qx = _mouseDifference.x;
+        Qy = _mouseDifference.y;
+
+        Debug.Log(_mouseDifference);
+
+        if (Qx <= -90)
+        {
+            Qx = -90;
+        }
+        else if (90 <= Qx)
+        {
+            Qx = 90;
+        }
+
         //x,yは逆
-        this._tr.eulerAngles += new Vector3(_mouseDifference.y * _sensitivity, _mouseDifference.x * _sensitivity, 0);
-        
-        //Debug.Log(_mouseDifference);
+        this._tr.rotation = Quaternion.Euler(Qy, Qx, _tr.rotation.z);
+        //eulerAnglesで+=してしまうとジンバルロックが起こり想定した挙動と異なってしまう
     }
 }
